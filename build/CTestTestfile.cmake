b72c2804a175d7bf1ed2c40e0541d45c3575305e
AloePacci <bfbdj92@gmail.com>
2021-04-04 19:53:40 +0200

    plantilla vacia

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/aloepacci/RosPackage/src
+# Build directory: /home/aloepacci/RosPackage/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
